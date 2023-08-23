374c033aabf78de26ffac7050101d313bed67d9f
wai phyo aung <waefioong@gmail.com>
2023-08-24 06:27:57 +0630

    Bootstrap Form

--- theblog/urls.py
+++ theblog/urls.py
@@ -1,8 +1,9 @@
 from . import views
 from django.urls import path
-from .views import HomeView, ArticleDetailView
+from .views import HomeView, ArticleDetailView, PostAddView
 
 urlpatterns = [
     path('',HomeView.as_view(), name='home'),
-    path('article/<int:pk>/',ArticleDetailView.as_view(), name ='article-detail')
+    path('article/<int:pk>/',ArticleDetailView.as_view(), name ='article-detail'),
+    path('add_post/',PostAddView.as_view(), name='add_post')
 ]
