aed3113fa5806809b8c0048e7daaea89851c03ed
waiphyoaung <waefioong@gmail.com>
2023-08-23 16:47:54 +0630

    simple blog

---
+++ theblog/urls.py
@@ -0,0 +1,8 @@
+from . import views
+from django.urls import path
+from .views import HomeView, ArticleDetailView
+
+urlpatterns = [
+    path('',HomeView.as_view(), name='home'),
+    path('article/<int:pk>/',ArticleDetailView.as_view(), name ='article-detail')
+]
